0ef2840a1e066e165a4d2bf11a171fbd738b011c
Iannaan <moshou5571228@gmail.com>
2023-10-22 17:19:27 +0800

    display balance rounding it to two decimal places.

--- financial_records/models.py
+++ financial_records/models.py
@@ -31,7 +31,8 @@ class FinancialRecord(models.Model):
 
     @property
     def balance(self):
-        return (self.credit - self.debit) * self.currency
+       # 使用 round 函数将结果四舍五入到两位小数
+      return round((self.credit - self.debit) * self.currency, 2)
     
 
     def __str__(self):
