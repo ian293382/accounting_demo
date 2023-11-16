7c7daa14a803380b3433dfda23d09cd7945bf9ee
Iannaan <moshou5571228@gmail.com>
2023-11-17 06:01:39 +0800

    create utils create message

--- utils/message_creater.py
+++ utils/message_creater.py
@@ -1,12 +1,53 @@
+
+
 def create_single_text_message(message):
     
     if message == 'ありがとう':
         message = 'どういたしまして！'
+    # test_message = [
+    #             {
+    #                 'type': 'text',
+    #                 'text': message,
+    #             }
+    #         ]
+    
+    
     test_message = [
-                {
-                    'type': 'text',
-                    'text': message,
-                }
-            ]
+        {
+            "type":"text",
+            "text":"Hello, user"
+        },
+        {
+            "type":"text",
+            "text":"May I help you?"
+        }
+    ]
     return test_message
 
+
+
+# def create_flex_message():
+#     flex_message =  [
+#     {
+#       "type": "flex",
+#       "altText": "This is a Flex Message",
+#       "contents": {
+#         "type": "bubble",
+#         "body": {
+#           "type": "box",
+#           "layout": "horizontal",
+#           "contents": [
+#             {
+#               "type": "text",
+#               "text": "Hello,"
+#             },
+#             {
+#               "type": "text",
+#               "text": "World!"
+#             }
+#           ]
+#         }
+#       }
+#     }
+#   ]
+#     return [flex_message]
