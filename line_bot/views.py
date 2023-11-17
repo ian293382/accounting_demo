35fc2e3cfdb6eb3a58ba140bccd1db42e92785ef
Iannaan <moshou5571228@gmail.com>
2023-11-17 22:17:45 +0800

    Implemented /b command to build financial record debit  on Line Bot.

--- line_bot/views.py
+++ line_bot/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 import json
 from django.views.decorators.csrf import csrf_exempt
@@ -33,9 +33,11 @@ from linebot.models import (
     CarouselTemplate, 
     CarouselColumn,
     PostbackAction,
-    MessageAction
+    MessageAction,
+    QuickReply, 
+    QuickReplyButton,
+    FlexSendMessage, BubbleContainer, BoxComponent, TextComponent
 )
-from linebot.models import TextSendMessage
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage, FollowEvent, UnfollowEvent
 from django.http import HttpResponse, HttpResponseBadRequest
@@ -45,10 +47,8 @@ line_bot_api = LineBotApi('kOeXM+8gIjtaPPKhrVPd+sqDCsjwtcN+1dJbFQBqbEX3A2i0hVW9o
 handler = WebhookHandler('fe1659d10566b01f16651411891a276c')
 
 
-
 from django.contrib.auth import  get_user_model
 User = get_user_model()
-
 
 
 @csrf_exempt
@@ -65,8 +65,6 @@ def index(request):
         user_id = events[0]['source']['userId']
         # print('user_id:', user_id)
         
-        line_user = User.objects.get(username=f"line_user_{user_id}",  password="dummy_password")
-      
         try:
             handler.handle(body, signature)
         except InvalidSignatureError:
@@ -76,20 +74,113 @@ def index(request):
     else:
         return HttpResponseBadRequest()
 
+from financial_records.models import Category,FinancialRecord
+from groups.models import Groups 
 
-# 訊息調整
-@handler.add(MessageEvent, message=TextMessage)
-def handle_text_message(event):
-    user_id = event.source.user_id
-    text_message = event.message.text
+def handle_command(event, user_id, command):
+    parts = command.split()
 
-    print('event', event)
-    # 在這裡進行你的資料解析或其他處理
-    # 例如，你可以將收到的文字訊息回傳給使用者
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=f'你說:{user_id}say{text_message}')
+    if len(parts) < 3:
+        return "指令格式不正確。使用：/b 100 一支筆 [描述] 或 /c 50000 收入 [描述]"
+
+    amount = parts[1]
+    record_name = parts[2]
+    description = " ".join(parts[3:]) if len(parts) > 3 else None
+
+    currency_index = parts.index('幣值') if '幣值' in parts else -1
+    currency = parts[currency_index + 1] if currency_index != -1 and len(parts) > currency_index + 1 else 1
+
+
+    user = get_object_or_404(User, username=f"line_user_{user_id}")
+    print(user)
+
+    group, created = Groups.objects.get_or_create(group_name="損益表（line自動創建)", created_by=user)
+ 
+    category, created = Category.objects.get_or_create(group=group, name="支出(由line自動創建)",created_by=user)
+   
+ 
+    record = FinancialRecord.objects.create(
+        group=group,
+        name=record_name,
+        description=description,
+        debit=amount,
+        currency=currency,
+        created_by=user
     )
+    record.category.add(category) 
+    return f"已创建支出记录：{record_name}，金额：{amount}，描述，剩下餘額：{record.balance}"
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    user_id = event.source.user_id
+    command = event.message.text
+
+    # 处理指令
+    response = handle_command(event, user_id, command)
+
+    # 回复用户
+    line_bot_api.reply_message(event.reply_token, TextSendMessage(text=response))
+
+
+# 表格範例
+
+# @handler.add(MessageEvent, message=TextMessage)
+# def handle_message(event):
+#     user_id = event.source.user_id
+#     text_message = event.message.text
+
+#     # 构建表格的数据
+#     table_data = [
+#         {"name": "John", "age": 25, "city": "New York"},
+#         {"name": "Alice", "age": 28, "city": "San Francisco"},
+#         {"name": "Bob", "age": 22, "city": "Los Angeles"},
+#     ]
+
+#     # 创建 Flex Message 中的 BubbleContainer
+#     bubble = BubbleContainer(
+#         body=BoxComponent(
+#             layout="vertical",
+#             contents=[
+#                 TextComponent(text="Table", weight="bold", size="xl"),
+#                 BoxComponent(
+#                     layout="vertical",
+#                     margin="lg",
+#                     spacing="sm",
+#                     contents=[
+#                         BoxComponent(
+#                             layout="horizontal",
+#                             spacing="sm",
+#                             contents=[
+#                                 TextComponent(text="Name", color="#aaaaaa", size="sm", flex=2),
+#                                 TextComponent(text="Age", color="#aaaaaa", size="sm", flex=1),
+#                                 TextComponent(text="City", color="#aaaaaa", size="sm", flex=2),
+#                             ],
+#                         )
+#                     ] +
+#                     [
+#                         BoxComponent(
+#                             layout="horizontal",
+#                             spacing="sm",
+#                             contents=[
+#                                 TextComponent(text=f"{data['name']}", size="sm", flex=2),
+#                                 TextComponent(text=f"{data['age']}", size="sm", flex=1),
+#                                 TextComponent(text=f"{data['city']}", size="sm", flex=2),
+#                             ],
+#                         )
+#                         for data in table_data
+#                     ],
+#                 ),
+#             ],
+#         ),
+#     )
+
+#     # 创建 Flex Message
+#     flex_message = FlexSendMessage(alt_text="Table", contents=bubble)
+
+#     # 使用 Line Bot API 发送 Flex Message
+#     line_bot_api.reply_message(event.reply_token, flex_message)
+
+
 
 
 #只要解除封鎖或加入群組都會觸發這條
