145b53367bb389d5953da7d40c2972b4d0ea6ed7
Iannaan <moshou5571228@gmail.com>
2023-11-27 10:54:34 +0800

    fix setting.py  google typo

--- accounting/settings.py
+++ accounting/settings.py
@@ -174,7 +174,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-ACTHENTICATION_BACKENDS = (
+AUTHENTICATION_BACKENDS = (
     'django.contrib.auth.backends.ModelBackend',
     'allauth.account.auth_backends.AuthenticationBackend',
 )
